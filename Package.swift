011e056ed5bc4decf8f342a3d6745b4b994cc160
disco-releaser <rodrigo@disconetwork.com>
2025-07-31 20:18:43 +0000

    Release 0.0.12

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DiscoSDK",
-            url: "https://github.com/co-op-commerce-inc/disco-ios-sdk/releases/download/0.0.10/DiscoSDK.xcframework.zip",
-            checksum: "366b4e9afea6a47e4b5f76f5bf85a6d050a9545fb3dc2751f035d93a5fd078b9"
+            url: "https://github.com/co-op-commerce-inc/disco-ios-sdk/releases/download/0.0.12/DiscoSDK.xcframework.zip",
+            checksum: "49121ad48ef0bcd2ac4aa5911a35b0ebfc4d9898194b561224c098a379191c37"
         )
     ]
 )
